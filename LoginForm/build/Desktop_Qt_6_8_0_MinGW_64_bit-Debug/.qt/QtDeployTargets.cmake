c74261d36314f25321e9eefe15113c6eeb486742
Berk Dwayne Onsoy <secure@elitepentest.com>
2025-01-08 08:27:41 +0100

    .

--- LoginForm/build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_LoginForm_FILE C:/Users/Unknown/Documents/Qt/LoginForm/build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/LoginForm.exe)
-set(__QT_DEPLOY_TARGET_LoginForm_RUNTIME_DLLS C:/Qt/6.8.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6Core.dll)
